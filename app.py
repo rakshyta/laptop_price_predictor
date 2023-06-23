655830baeaad7877a6cf641280e49a0d95cee092
unknown <sadanshive.rakshita@gmail.com>
2023-06-23 13:02:34 +0530

    first commit

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ type_ = st.selectbox('Type', ['Ultrabook', 'Notebook', 'Netbook', 'Gaming', '2 i
 ram = st.selectbox('RAM(in GB)', [2,4,6,8,12,16,24,32,64])
 
 # weight
-weight = st.number_input('Weight of the Laptop')
+weight = st.number_input('Weight of the Laptop', 0.0, 5.0, 0.01)
 
 # Touchscreen
 touchscreen = st.selectbox('Touchscreen', ['No','Yes'])
@@ -35,7 +35,7 @@ touchscreen = st.selectbox('Touchscreen', ['No','Yes'])
 ips = st.selectbox('IPS', ['No','Yes'])
 
 # screen size
-screen_size = st.number_input('Screen Size')
+screen_size = st.number_input('Screen Size', 0.0, 880000.0)
 
 # resolution
 resolution = st.selectbox('Screen Resolution', ['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
@@ -88,7 +88,8 @@ if st.button('Predict Price'):
     Y_res = int(resolution.split('x')[1])
     ppi = ((X_res**2) + (Y_res**2))**0.5/screen_size
     query = np.array([company,type_,ram,weight,touchscreen,ips,ppi,cpu,hdd,ssd,gpu,os])
-
-    query = query.reshape(1,12)
     pipe = clf.predict(query)[0]
+    
+    query = query.reshape(1,12)
+    
     st.title("The predicted price of this configuration is " + str(int(np.exp(pipe))))
